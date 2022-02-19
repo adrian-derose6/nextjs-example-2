8d632d9466efc7e942a358b4b85565f7920bb0ff
Adrian DeRose <adrian.derose6@gmail.com>
2022-02-18 20:47:22 -0600

    Use getStaticProps to initialize props on server

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY_MEETUPS = [
@@ -20,14 +19,19 @@ const DUMMY_MEETUPS = [
 	},
 ];
 
-function HomePage() {
-	const [loadedMeetups, setLoadedMeetups] = useState([]);
-	useEffect(() => {
-		// send http request and fetch data
-		setLoadedMeetups(DUMMY_MEETUPS);
-	}, []);
+function HomePage(props) {
+	return <MeetupList meetups={props.meetups} />;
+}
 
-	return <MeetupList meetups={loadedMeetups} />;
+export async function getStaticProps() {
+	// fetch data from API
+
+	// Always return object
+	return {
+		props: {
+			meetups: DUMMY_MEETUPS,
+		},
+	};
 }
 
 export default HomePage;
